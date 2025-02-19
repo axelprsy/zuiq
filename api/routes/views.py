70bad62b98dcd7b90a1922f078a2634e2edb16fc
Lunity <axel@parsy.fr>
2025-02-19 16:36:40 +0100

    feat: ajout de la fonction generate_quizz avec ia

--- api/routes/views.py
+++ api/routes/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 import flask_restful as restful
 from flask_cors import CORS
+from models.ai_quizz import GenerateQuizz
 
 from models.users import User, Users
 from models.quizz import Quizz
@@ -12,5 +13,6 @@ api = restful.Api()
 api.add_resource(User, "/user")
 api.add_resource(Users, "/users")
 api.add_resource(Quizz, "/quizz")
+api.add_resource(GenerateQuizz, "/generate_quizz")
 
 api.init_app(api_blueprint)
