3e5bd9b6cacca8c86788e512bea7e8e2208a4f3c
Lunity <axel@parsy.fr>
2025-02-20 21:24:03 +0100

    feat: ajout du bouton logout et supprimer mon compte

--- front/pages/js/profile.js
+++ front/pages/js/profile.js
@@ -1,3 +1,27 @@
 document.getElementById('id').innerText = `ID : ${localStorage.getItem('user_id')}`;
 document.getElementById('username').innerText = `Nom d'utilisateur : ${localStorage.getItem('username')}`;
 document.getElementById('email').innerText = `Email : ${localStorage.getItem('email')}`;
+
+function logout() {
+    localStorage.clear();
+    window.location.href = '/';
+    window.location.replace("../../auth/html/login.html");
+}
+
+async function delete_my_account() {
+    const formdata = new FormData();
+    formdata.append("user_id", localStorage.getItem('user_id'));
+
+    const requestOptions = {
+    method: "DELETE",
+    body: formdata,
+    redirect: "follow"
+    };
+
+    await fetch("http://127.0.0.1:5000/user", requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.error(error));
+
+    window.location.replace("../../auth/html/signup.html");
+}
