dc02044477a533a59ab13477015b6c69652e2e02
Lunity <axel@parsy.fr>
2025-02-24 23:38:29 +0100

    feat:

--- front/quizz/js/start-quizz.js
+++ front/quizz/js/start-quizz.js
@@ -5,7 +5,8 @@ const socket = io('http://localhost:5050', {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-    socket.emit('createSession');
+    const user_id = localStorage.user_id
+    socket.emit('createSession', {"user_id": user_id});
     socket.on('sessionCreated', ({ code }) => {
         sessionCodeDisplay.textContent = `Code de session : ${code}`;   
     }); 
@@ -29,19 +30,25 @@ startQuizzButton.addEventListener('click', () => {
     fetch("http://127.0.0.1:5000/quizz?user_id=4", requestOptions)
       .then((response) => response.json())
         .then((result) => {
-            // console.log(result.quizz[0].questions[0].title)
-            const code = sessionCodeDisplay.textContent.split(': ')[1];
-            const question = result.quizz[0].questions[0].title
-            const answers = result.quizz[0].questions[0].answers
-            const quizz_id = result.quizz[0].quizz_id
-            const question_id = result.quizz[0].questions[0].question_id
+            const input_number_of_question = document.getElementById("numberOfQuestion")
+            const number_of_question = parseInt(input_number_of_question.value)
+            const questions = result.quizz[0]["questions"]
+                if (questions.length != number_of_question) {
+                const code = sessionCodeDisplay.textContent.split(': ')[1];
+                const title = questions[number_of_question].title
+                const answers = questions[number_of_question].answers
+                const quizz_id = result.quizz[0].quizz_id
+                const question_id = questions[number_of_question].question_id
 
-            console.log(question)
-
-            if (code && question) {
-                socket.emit('sendQuestion', { code, question, answers, quizz_id, question_id });
-                // questionInput.value = ''; // Réinitialise l'input
+                if (code && title) {
+                    console.log(questions.length, number_of_question)
+                    socket.emit('sendQuestion', { "code":code, "question":title, "answers":answers, "quizz_id": quizz_id, "question_id":question_id });
+                    input_number_of_question.value = number_of_question+1
+                }
+            } else {
+                console.log("fin du quizz")
             }
+        
         })
 });
 
