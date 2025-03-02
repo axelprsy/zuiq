2ad4ad0075c9a3883e39baff9c5e05ee200211a5
Lunity <axel@parsy.fr>
2025-03-02 19:17:32 +0100

    fix: utilisation de ollama

--- api/models/ai_quizz.py
+++ api/models/ai_quizz.py
@@ -1,7 +1,7 @@
 import json
+import os
 from datetime import datetime
-from ollama import chat
-from ollama import ChatResponse
+from ollama import Client
 from flask import jsonify
 from flask_restful import Resource, reqparse
 
@@ -17,12 +17,16 @@ class GenerateQuizz(Resource):
         parser.add_argument("public", required=True, location="args")
         args = parser.parse_args()
 
-        response: ChatResponse = chat(model='mistral', messages=[
-        {
+        client = Client(
+        host=os.getenv('OLLAMA_URL', 'http://127.0.0.1:11434'),
+        headers={'x-some-header': 'some-value'}
+        )
+        response = client.chat(model='mistral', messages=[
+            {
             'role': 'user',
             'content': f"""
             Crée un quiz amusant et éducatif sur le thème {args['theme']} avec {args['number_of_questions']} questions.
-            Chaque question doit comporter un énoncé clair et concis, avec 4 propositions : A, B, C et D, et précise la bonne réponse.
+            Chaque question doit comporter un énoncé clair et concis, avec 4 propositions et précise la bonne réponse.
             Les questions doivent être adaptées à des {args['public']}.
             Je veux que ta réponse soit un JSON exactement sous cette forme :
 
@@ -49,5 +53,4 @@ class GenerateQuizz(Resource):
         ])
         quizz=json.loads(response.message.content)
         quizz["quizz"]["created_at"]=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
         return jsonify({"status": 200, "quizz": quizz})
