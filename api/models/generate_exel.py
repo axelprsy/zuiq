80f76dcbad20cfe737fdf189a94db1de2b6abdb7
Lunity <axel@parsy.fr>
2025-03-08 18:24:29 +0100

    feat: generation de fichier exel avec le score

---
+++ api/models/generate_exel.py
@@ -0,0 +1,41 @@
+import json
+import os
+import pandas as pd
+import ast
+from flask import jsonify, send_file, make_response
+from flask_restful import Resource, reqparse
+
+
+class GenerateExel(Resource):
+    def get(self):
+        """
+        Générer un fichier exel avec le resultat de la session.
+        """
+        parser = reqparse.RequestParser()
+        parser.add_argument("session_data", required=True, location="args")
+        args = parser.parse_args()
+        session_data = args["session_data"]
+        print(session_data)
+        try:
+            python_list = ast.literal_eval(session_data)
+            data = [{key: value for key, value in item.items() if key != "user_id"} for item in python_list]
+        except (ValueError, SyntaxError) as e:
+            return jsonify({"status": 400, "message": "Invalid input format. Please provide a valid Python-like list."})
+
+        df = pd.DataFrame(data)
+
+        excel_file = "result.xlsx"
+        df.to_excel(excel_file, index=False)
+
+        try:
+            response = make_response(send_file(
+                excel_file,
+                as_attachment=True,
+                download_name=excel_file,
+                mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            ))
+            response.headers["Content-Disposition"] = f"attachment; filename={excel_file}"
+            return response
+        finally:
+            if os.path.exists(excel_file):
+                os.remove(excel_file)
--- api/routes/views.py
+++ api/routes/views.py
@@ -5,6 +5,7 @@ from flask_cors import CORS
 from models.users import User, Users
 from models.quizz import Quizz
 from models.session import Session
+from models.generate_exel import GenerateExel
 
 api_blueprint = Blueprint("api,", __name__)
 CORS(api_blueprint)
@@ -14,6 +15,7 @@ api.add_resource(User, "/user")
 api.add_resource(Users, "/users")
 api.add_resource(Quizz, "/quizz")
 api.add_resource(Session, "/session")
+api.add_resource(GenerateExel, "/generate_exel")
 
 try: # si ollama n'est pas installé generate quizz n'est pas ajouté a l'api
     from models.ai_quizz import GenerateQuizz
