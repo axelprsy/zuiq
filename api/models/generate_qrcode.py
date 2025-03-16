dceb090176a51d26172fc55258f2a181ae8b1663
Lunity <axel@parsy.fr>
2025-03-16 20:11:42 +0100

    feat: ajout d'un qr code pour rejoindre une session

---
+++ api/models/generate_qrcode.py
@@ -0,0 +1,37 @@
+import qrcode
+import base64
+from io import BytesIO
+from flask import jsonify, send_file, make_response
+from flask_restful import Resource, reqparse
+
+
+class GenerateQrCode(Resource):
+    def get(self):
+        """
+        Générer un fichier exel avec le resultat de la session.
+        """
+        parser = reqparse.RequestParser()
+        parser.add_argument("session_code", required=True, location="args")
+        args = parser.parse_args()
+        session_code = args["session_code"]
+
+        # Génération du QR code
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(session_code)
+        qr.make(fit=True)
+
+        img = qr.make_image(fill_color="black", back_color="white")
+
+        buffer = BytesIO()
+        img.save(buffer, format="PNG")
+        buffer.seek(0)
+
+        img_base64 = base64.b64encode(buffer.read()).decode()
+        data_url = f"data:image/png;base64,{img_base64}"
+        
+        return jsonify({"qr_code": data_url})
