56f6c7e9253107a720f56ff4af8a8864cc911b9b
Lunity <axel@parsy.fr>
2025-03-24 23:40:08 +0100

    test: start file windows

--- start.ps1
+++ start.ps1
@@ -1,19 +1,9 @@
-# Récupérer l'adresse IP locale (non 127.0.0.1)
-$ipAddress = (Get-NetIPAddress -AddressFamily IPv4 | Where-Object {
-    $_.IPAddress -notlike "127.*" -and $_.InterfaceAlias -notlike "*Loopback*"
-}).IPAddress
+$ipconfig = Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.InterfaceAlias -notlike "*Loopback*" }
 
-# Vérifier si une adresse IP a été trouvée
-if (-not $ipAddress) {
-    Write-Host "Impossible de récupérer l'adresse IP" -ForegroundColor Red
-    exit 1
-}
-
-# Afficher l'adresse IP à l'utilisateur
 Write-Host "Adresse IP détectée : $ipAddress" -ForegroundColor Green
 
 # Exporter l'adresse IP en tant que variable d'environnement
-$env:HOST_IP = $ipAddress
+$env:HOST_IP = $ipconfig.IPAddress
 
 # Démarrer Docker Compose avec la commande requise
 docker compose up --build
